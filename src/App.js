7e8b83c76e2b3516f0e8dc3b0cd1c3a546f43dd8
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-22 18:37:53 +0300

    refactor persont component

--- src/App.js
+++ src/App.js
@@ -38,11 +38,24 @@ class App extends Component {
     }
 
     render() {
+
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointor',
+
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is realyy working!</p>
-                <button onClick={() => this.switchNameHandler('Maximilian!!')}>SwitchName</button>
+                <button
+                    style={style}
+                    onClick={() => this.switchNameHandler('Maximilian!!')}
+                >SwitchName</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}/>
