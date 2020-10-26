7559a1b8a6af32b8fd8399bec7e1ec26241ecbc6
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-26 09:16:32 +0200

    add PropTypes to oerson component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react';
 import classes from './Person.css'
 import Auxiliary from "../../../hoc/Auxiliary";
 import withClass from '../../../hoc/withClass';
+import PropTypes from 'prop-types'
 
 class Person extends Component {
 
@@ -24,6 +25,14 @@ class Person extends Component {
 
         )
     }
+
+}
+
+Person.propTypes = {
+    click: PropTypes.fun,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.fun
 
 }
 
