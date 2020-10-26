2dbf1720ac21c971e2393921d7f9d94bf63584cb
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-26 12:27:51 +0200

    - add contex to component didmount

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -13,8 +13,12 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType =AuthContext;
+
+
     componentDidMount() {
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated)
     }
 
     render() {
@@ -26,11 +30,7 @@ class Person extends Component {
 
         return (
             <Auxiliary>
-                <AuthContext.Consumer>{
-                    context=>context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-
-                }
-                </AuthContext.Consumer>
+                {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>                }
 
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                     <p>{this.props.children}</p>
